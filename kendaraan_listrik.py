edffa6ee5d3879b10ce6afebb30a9dd6e766a20d
Satrio Sani Sadewo <113606485+satriosani@users.noreply.github.com>
2022-10-23 20:31:01 +0700

    Update kendaraan_listrik.py

--- kendaraan_listrik.py
+++ kendaraan_listrik.py
@@ -15,11 +15,11 @@ with cola:
     st.write("Krisis lingkungan hidup mulai dirasakan di berbagai belahan dunia. Tidak dapat dipungkiri bahwa hasil emisi gas karbon yang bersumber dari fossil termasuk salah satu penyebabnya. Bagaimana dengan di Indonesia? Melalui laporan Inventarisasi Emisi GRK (Gas Rumah Kaca) Bidang Energi yang diterbitkan oleh Pusat Data dan Teknologi Informasi Kementerian ESDM tahun 2020, dipaparkan bahwa konsumsi bahan bakar di Indonesia mengalami peningkatan dengan rata-rata pertumbuhan sebesar 3,71% per Tahun (Kementerian ESDM, 2020: 15). Kemudian untuk emisi yang dihasilkan, jika dibandingkan dengan tahun sebelumnya, emisi pada tahun 2019 mengalami kenaikan sebesar 7.13% dengan rata-rata peningkatan sebesar 4,32% per tahun. Kategori paling banyak menyumbang emisi adalah industri produsen energi sebesar 43,83% diikuti sektor transportasi sebesar 24,64% (Kementerian ESDM, 2020: 28-29). Berbagai upaya terus dilakukan untuk melawan permasalahan ini. Salah satunya adalah dengan memanfaatkan listrik sebagai tenaga penggerak kendaraan, yang diklaim sebagai kendaraan ramah lingkungan. Benarkah?")
 
 with colb:
-    st.image(esdm1.PNG, caption='Konsumsi Bahan Bakar per Kategori')
+    st.image('esdm1.PNG', caption='Konsumsi Bahan Bakar per Kategori')
     st.caption("sumber : Inventarisasi Emisi GRK Bidang Energi, Kementerian ESDM RI 2020")
 
 with colc:
-    st.image(esdm2.PNG, caption='Kontribusi Setiap Kategori dalam Emisi GRK Tahun 2019 di Indonesia')
+    st.image('esdm2.PNG', caption='Kontribusi Setiap Kategori dalam Emisi GRK Tahun 2019 di Indonesia')
     st.caption("sumber : Inventarisasi Emisi GRK Bidang Energi, Kementerian ESDM RI 2020")
     
 st.subheader("Data Penjualan Mobil Listrik")
